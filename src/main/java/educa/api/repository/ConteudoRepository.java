ced75e80a5a1524d500e868febd64285f7a2a2b1
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-10-08 19:12:40 -0300

    adicionando conceito de paginação no método read da classe conteudoController

--- src/main/java/educa/api/repository/ConteudoRepository.java
+++ src/main/java/educa/api/repository/ConteudoRepository.java
@@ -1,7 +1,12 @@
 package educa.api.repository;
 
 import educa.api.domain.Conteudo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConteudoRepository extends JpaRepository<Conteudo, Integer> {
+
+    Page<Conteudo> findByTitulo(String titulo, Pageable paginacao);
+
 }
