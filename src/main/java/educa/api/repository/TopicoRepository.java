164d37a39638a10bc1e1c8027aa214692f4f58e1
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-10-09 11:58:49 -0300

    paginação da classe Topicos e ajustes no application.properties

--- src/main/java/educa/api/repository/TopicoRepository.java
+++ src/main/java/educa/api/repository/TopicoRepository.java
@@ -1,7 +1,11 @@
 package educa.api.repository;
 
 import educa.api.domain.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Integer> {
+    Page<Topico> findByTitulo(String titulo, Pageable paginacao);
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO AVALIACAO(avaliacao) VALUES
+('Gostei do conteúdo');
+
+INSERT INTO conteudo(artigo, data_criacao, tempo_estimado, texto, titulo, url) VALUES
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br'),
+('teste de artigos', now(), 10, 'teste campo texto', 'teste campo titulo', 'www.teste.com.br');
+
+INSERT INTO topico(data_criacao, descricao, status, titulo) VALUES
+(now(), 'teste descricao', 'OKOKO', 'Teste Título');
