65eaa41bc272be84a06fefa546e4face09bb606d
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-10-23 18:51:05 -0300

    allowedOrigins permitindo acesso a app react

--- src/main/java/educa/api/config/security/SecurityConfigurations.java
+++ src/main/java/educa/api/config/security/SecurityConfigurations.java
@@ -32,7 +32,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter impleme
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
     @Override
