1f6cf44986afab1481e1690009847cc8c10332b6
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-11-19 12:27:58 -0300

    implementação de fila e pilha

--- src/main/java/educa/api/controller/EstudanteController.java
+++ src/main/java/educa/api/controller/EstudanteController.java
@@ -6,11 +6,15 @@ import educa.api.request.EstudanteRequest;
 import educa.api.repository.PerfilRepository;
 import educa.api.repository.UsuarioRepository;
 import educa.api.response.UsuarioEstudanteResponse;
+import educa.api.utils.FilaObj;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -65,6 +69,24 @@ public class EstudanteController {
             estudante.adicionarPerfil(perfilEstudante);
             repository.save(estudante);
             return ResponseEntity.status(200).body(repository.getEstudanteById(usuario.getIdUsuario()).get());
+    }
+
+    private FilaObj<Usuario> filaAgendar;
+
+    @DeleteMapping()
+    public ResponseEntity<Void> delete(@AuthenticationPrincipal Usuario usuario) {
+        filaAgendar.insert(usuario);
+        throw new ResponseStatusException(
+                HttpStatus.OK,
+                "Processo de exclusão da conta em andamento, prazo de até 24h"
+        );
+    }
+
+    @Scheduled(cron = "0 0 24")
+    public void deletarAccount() {
+        for (int i = 0; i < filaAgendar.getTamanho(); i++) {
+            repository.deleteById(filaAgendar.pool().getIdUsuario());
+        }
     }
 
 }
