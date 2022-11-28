fe1446e0b042c26e5d3f73eb53fcb3eb1071fc4b
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-11-28 11:23:34 -0300

    ajsutando rotas

--- src/main/java/educa/api/config/security/CorsConfigurations.java
+++ src/main/java/educa/api/config/security/CorsConfigurations.java
@@ -12,7 +12,6 @@ public class CorsConfigurations implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("educa-tech.eastus.cloudapp.azure.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
